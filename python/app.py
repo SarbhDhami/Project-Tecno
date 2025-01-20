b5a63f541c032278a19363cbb23c40ce3ab6ce5f
RuslanArtemciuc <ruslandenis97@gmail.com>
2025-01-20 08:50:16 +0100

    ho creato l'app flask

---
+++ python/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+import os
+
+import os
+from flask import Flask
+
+template_dir = os.path.abspath('../html')
+app = Flask(__name__, template_folder=template_dir)
+
+@app.route('/')
+def public_home():
+    return render_template('public/index.html')
+
+@app.route('/admin')
+def admin_home():
+    return render_template('admin/index.html')
+
+
+if __name__ == '__main__':
+    debug_mode = os.environ.get('FLASK_DEBUG', 'False').lower() in ['true', '1', 't']
+    app.run(debug=debug_mode, host='0.0.0.0')
